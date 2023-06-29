a0c3ebf93a59175aba744c6f317a4cafe329a4cf
Olga Klimenko <oklimenko92@mail.ru>
2023-06-29 19:20:18 +0700

    add models and admin panel

--- donat_pool/core/admin.py
+++ donat_pool/core/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from .models import Author, Category, Fundraising, CompletedFundraising, Tag
 
-# Register your models here.
+
+class AuthorInline(admin.TabularInline):
+    model = Author
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+class TagInline(admin.StackedInline):
+    model = Fundraising.tags.through
+
+class FundraisingInline(admin.TabularInline):
+    model = Fundraising
+    readonly_fields = ["path",]
+
+class CompletedFundraisingInline(admin.TabularInline):
+    model = CompletedFundraising
+    readonly_fields = ["targetAmount", "raisedAmount"]
+    
+class AuthorAdmin(admin.ModelAdmin):
+    readonly_fields = ["pkh",]
+    list_display = (
+        'trusted', 
+        'untrustworthy', 
+        'created_at',
+        )
+    inlines = [FundraisingInline]
+    search_fields = ['pkh',]
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        )
+    search_fields = ['name',]
+
+class FundraisingAdmin(admin.ModelAdmin):
+    readonly_fields = ["path",]
+    list_display = (
+        'created_at',
+        'category',
+        'verified',
+        'promoted',
+        'main',
+        )
+    inlines = [TagInline, CompletedFundraisingInline]
+    search_fields = ['path',]
+
+class CompletedFundraisingAdmin(admin.ModelAdmin):
+    list_display = (
+        'completedAt',
+        'targetAmount',
+        'raisedAmount',
+        )
+    search_fields = ['fundraising__path',]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        )
+    search_fields = ['name',]
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Fundraising, FundraisingAdmin)
+admin.site.register(CompletedFundraising, CompletedFundraisingAdmin)
+admin.site.register(Tag, TagAdmin)
--- donat_pool/core/models.py
+++ donat_pool/core/models.py
@@ -1,3 +1,105 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+
+class Author(models.Model):
+    pkh = models.CharField(max_length=255, primary_key=True)
+    trusted = models.BooleanField(default=False)
+    untrustworthy = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "author"
+        verbose_name_plural = "authors"
+
+    def __str__(self):
+        return self.pkh
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    background = models.ImageField(
+        upload_to=("images/categories"), 
+        blank=True, 
+        null=True,
+        )
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+class Fundraising(models.Model):
+    path = models.CharField(max_length=255, primary_key=True)
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE, 
+        related_name="fundraisings", 
+        related_query_name="fundraising",
+        )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        related_name="fundraisings", 
+        related_query_name="fundraising",
+        blank=True,
+        null=True,
+        )
+    description = models.CharField(
+        max_length=255, 
+        blank=True,
+        null=True,
+        )
+    image = models.ImageField(
+        upload_to=("images/fundraisings"), 
+        blank=True, 
+        null=True,
+        )
+    verified = models.BooleanField(default=False)
+    promoted = models.BooleanField(default=False)
+    main = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "fundraising"
+        verbose_name_plural = "fundraisings"
+
+    def __str__(self):
+        return self.path
+
+class CompletedFundraising(models.Model):
+    fundraising = models.OneToOneField(
+        Fundraising,
+        on_delete=models.CASCADE,
+        related_name="info",
+        related_query_name="info"
+        )
+    targetAmount = models.BigIntegerField()
+    raisedAmount = models.BigIntegerField()
+    completedAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "completed fundraising"
+        verbose_name_plural = "completed fundraisings"
+
+    def __str__(self):
+        return self.fundraising.path
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    fundraisings = models.ManyToManyField(
+        Fundraising, 
+        related_name="tags", 
+        related_query_name="tag",
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = "tag"
+        verbose_name_plural = "tags"
+         
+    def __str__(self):
+        return self.name
