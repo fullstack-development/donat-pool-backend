25e4b230fad8a4c26836703f8c3143f14ef8ca7c
Olga Klimenko <oklimenko92@mail.ru>
2023-07-06 19:20:11 +0700

    fix site settings serializer

--- donat_pool/core/serializers.py
+++ donat_pool/core/serializers.py
@@ -5,12 +5,12 @@ from .models import SiteSettings, Category, Tag
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name', 'background',]
+        fields = ['id', 'name', 'background',]
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['name',]
+        fields = ['id', 'name',]
 
 class SiteSettingsSerializer(serializers.ModelSerializer):
     categories = CategorySerializer(read_only=True, many=True)
